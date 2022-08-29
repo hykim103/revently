fed9b89a073a48f3d80e46d00e23c63eaf0d5ab2
ethan lane <ethan.lane@outlook.co.nz>
2022-08-29 16:17:47 +0100

    restaurants validations

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,3 +1,7 @@
 class Restaurant < ApplicationRecord
   belongs_to :user
+  has_many :menus
+  validates :name, :address, :cuisine, phone_number:, presence: true
+  validates :phone_number, format: { with: /^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$/,
+    message: "only allows valid phone numbers" }
 end
