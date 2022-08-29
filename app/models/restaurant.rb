939460ab925c29d1a2be45e257d5d44154c2f398
ethan lane <ethan.lane@outlook.co.nz>
2022-08-29 16:30:37 +0100

    reseverations through users

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,6 +1,8 @@
 class Restaurant < ApplicationRecord
   belongs_to :user
-  has_many :menus
+  has_many :menus, dependent: :destory
+  has_many :reservations, through: :users
+
   validates :name, :address, :cuisine, phone_number:, presence: true
   validates :phone_number, format: { with: /^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$/,
     message: "only allows valid phone numbers" }
