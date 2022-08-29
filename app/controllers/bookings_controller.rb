1695fb956be2fd9c94b543d1c9e15d8db53732b7
ethan lane <ethan.lane@outlook.co.nz>
2022-08-29 17:32:51 +0100

    controller half done

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,10 +1,18 @@
 class BookingsController < ApplicationController
   def create
-    @booking = Restaurant.new(booking_params)
+    @booking = Booking.new(booking_params)
     @booking.save
   end
 
   def destroy
-    
+    @booking = Booking.find(params[:id])
+    @booking.destroy
+    redirect_to user_path, status: :see_other
+  end
+
+  private
+
+  def booking_params
+    params.require(:booking).permit(:booking_date)
   end
 end
