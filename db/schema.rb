c44f9db42c7c5b3e4bae358180193bd34f1a7700
HaYeon Kim <kimhy8376@gmail.com>
2022-08-29 15:38:29 +0100

    Fixed User controller and create a menu model

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_08_29_140733) do
+ActiveRecord::Schema[7.0].define(version: 2022_08_29_143506) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "menus", force: :cascade do |t|
+    t.string "name"
+    t.string "description"
+    t.string "category"
+    t.float "price"
+    t.bigint "restaurant_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["restaurant_id"], name: "index_menus_on_restaurant_id"
+  end
 
   create_table "restaurants", force: :cascade do |t|
     t.string "name"
@@ -41,5 +52,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_08_29_140733) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "menus", "restaurants"
   add_foreign_key "restaurants", "users"
 end
