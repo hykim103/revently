ce232444d899de43abfca7978b2d79ea753dd01c
ethan lane <ethan.lane@outlook.co.nz>
2022-08-30 11:28:28 +0100

    fixed reservations view page and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,15 @@
 Rails.application.routes.draw do
-  get 'bookings/create'
-  get 'bookings/destroy'
+  get 'reservation/destroy'
+
   devise_for :users
   root to: "pages#home"
-  get 'user_bookings', to: 'bookings#user_bookings'
+  get 'user_bookings', to: 'reservations#user_bookings'
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
-  resources :restaurants
+  resources :restaurants do
+    resources :reservations, only: [:create]
+  end
 end
