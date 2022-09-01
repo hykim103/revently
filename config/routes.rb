7270dd4b6771bbbb0e36eb3530fd3c12dc00b10e
Julia Yu <jjjuliayyyu@gmail.com>
2022-09-01 11:07:49 +0100

    footer positioning, navbar button logic, host-restaurants page

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   root to: "pages#home"
   get 'user_bookings', to: 'reservations#user_bookings'
   get 'confirmation', to: 'reservations#create'
+  get 'host_restaurants', to: 'restaurants#host_restaurants'
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
